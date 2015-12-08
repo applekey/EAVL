5e94b49447ec8c9635d33812e9f4ad048f93c251
Roba Binyahib <roba.binyahib@kaust.edu.sa>
2015-12-07 20:30:10 -0800

    changes

--- EAVL-rayTracer/test/myCamera.py
+++ EAVL-rayTracer/test/myCamera.py
@@ -4,10 +4,11 @@ import math
 x = 69.782;
 z = 0.0;
 y = 0.0;
-for i in range(1,180):
-	theata = 180.0/i;
-	newX = x * math.cos(theata) + z * math.sin(theata);
-	newZ = z * math.cos(theata) - x * math.sin(theata);
-	print newX, y, newZ
-	x = newX;
-	z = newZ;
+for i in range(0,180):
+	#theata = 180.0/i;
+	#newX = x * math.cos(theata) + z * math.sin(theata);
+	#newZ = z * math.cos(theata) - x * math.sin(theata);
+	rad = i*math.pi/180.0
+	print rad
+	#x = newX;
+	#z = newZ;
